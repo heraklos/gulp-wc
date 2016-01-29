ee3b18713b28b333692e00a326e5d26ba39ccd6a
Kajiwara <kajiwara@mtl.t.u-tokyo.ac.jp>
2016-01-30 02:41:27 +0900

    make it display lines of code

---
+++ index.js
@@ -0,0 +1,43 @@
+'use strict';
+
+var es = require('event-stream');
+var gutil = require('gulp-util');
+var log = gutil.log;
+var colors= gutil.colors;
+var PluginError = gutil.PluginError;
+
+function gulpWc() {
+
+    var codeInUtf = "",
+        linesOfCode = 0,
+        wholeLinesOfCode = 0,
+        stringForTotal = "Total";
+
+    function countLines(file) {
+
+        var path = file.path.split('/'),
+            filename = path[path.length - 1];
+
+        if (file.isNull()) {
+            throw new PluginError('gulp-wc', 'Missing file for gulp-wc');
+        }
+
+        codeInUtf = file.contents.toString('utf8');
+        linesOfCode = codeInUtf.split('\n').length;
+        wholeLinesOfCode += linesOfCode;
+
+        printResult(filename);
+
+    }
+
+    function printResult(filename) {
+        filename = filename ? filename : stringForTotal;
+        var lines = filename !== stringForTotal ? linesOfCode : wholeLinesOfCode;
+        log(colors.green(filename +" :" + lines + " lines"));
+
+    }
+
+    return es.through(countLines, printResult);
+}
+
+module.exports = gulpWc;
